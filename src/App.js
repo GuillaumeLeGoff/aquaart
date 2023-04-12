2f24082d88658445c8a8e77985ad664612985646
GuillaumeLeGoff <114984796+GuillaumeLeGoff@users.noreply.github.com>
2023-04-12 11:46:59 +0200

    app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-import CompetitionPage from "./page/CompetitionPage";
+
 import WebSocketApp from "./page/WebSocketPage";
 
 function App() {
   return (
     <div style={{ backgroundColor: "#f5f5f5", minHeight: "100vh" }}>
       <WebSocketApp />
-     {/*  <CompetitionPage /> */}
+
     </div>
   );
 }
